48074c6e91c7eab6c9bc2c96d6d90fef744fbda2
Rich Lewis <richlewis42@users.noreply.github.com>
2018-09-13 09:17:12 +0100

    Adds test and format commands to setup.py
    Adds a format command to run black, and configures the test command
    to run pytest.

--- setup.py
+++ setup.py
@@ -1,4 +1,40 @@
-from setuptools import find_packages, setup
+import sys
+from setuptools import find_packages, setup, Command
+from setuptools.command.test import test as TestCommand
+
+
+class BlackCommand(Command):
+    """ Command to format code with Black """
+
+    user_options = [("black-args-", "a", "Arguments to pass to black")]
+
+    def initialize_options(self):
+        self.black_args = "."
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import black, shlex
+
+        errno = black.main(shlex.split(self.black_args))
+
+
+class PyTestCommand(TestCommand):
+    """ Command to run tests using pytest """
+
+    user_options = [("pytest-args-", "a", "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        super().initialize_options()
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import shlex, pytest
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
 
 setup(
     name="element-case",
@@ -12,4 +48,6 @@ setup(
     },
     packages=find_packages(),
     license="MIT",
+    tests_require=["pytest"],
+    cmdclass={"test": PyTestCommand, "format": BlackCommand},
 )
